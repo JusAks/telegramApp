e1ed09ecf122b605203379d0b88ca94152c34d5b
JusAks <anime070299@gmail.com>
2024-07-31 14:36:06 +0300

    2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {TonConnectUIProvider} from "@tonconnect/ui-react";
+import {BrowserRouter} from "react-router-dom";
+
+const link = 'https://t.me/AksTestMiniAppReact_bot/play'
+//const link = 'https://t.me/testWebAppAks_bot/start'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <TonConnectUIProvider
+          manifestUrl="https://justaks1.netlify.app/tonconnect-manifest.json"
+          actionsConfiguration={{
+              twaReturnUrl: link
+          }}
+      >
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </TonConnectUIProvider>
   </React.StrictMode>,
 )
